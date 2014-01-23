908f7a3c18b1a50921ecb3113c5f6d3c661b6739
Joung Daun <zxc3132@hanmail.net>
2014-01-23 14:31:05 +0900

    Initial Commit

---
+++ Bowling300Tests/Bowling300Tests.m
@@ -0,0 +1,34 @@
+//
+//  Bowling300Tests.m
+//  Bowling300Tests
+//
+//  Created by SDT-1 on 2014. 1. 23..
+//  Copyright (c) 2014년 T. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Bowling300Tests : XCTestCase
+
+@end
+
+@implementation Bowling300Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end

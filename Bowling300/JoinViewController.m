87be97fc886ae2fbe7dc282c56027c9891980b4c
Joung Daun <zxc3132@hanmail.net>
2014-02-06 18:06:38 +0900

    임시저장..
    ..

--- Bowling300/JoinViewController.m
+++ Bowling300/JoinViewController.m
@@ -33,18 +33,33 @@
     [self.view endEditing:YES];
 }
 - (IBAction)pressSaveButton:(id)sender {
+    //build an info object and convert to json
+    NSURL *url = [NSURL URLWithString:@"http://bowling.pineoc.cloulu.com/user/sign"];
+    NSDictionary *newDatasetInfo = @{@"email": @"i_co1022@naver.com",@"name":@"Joung Daun",@"pwd":@"merong",@"proPhoto":@"",@"ballPhoto":@""};
+    NSError *error;
+    //convert object to data
+    NSData* jsonData = [NSJSONSerialization dataWithJSONObject:newDatasetInfo options:kNilOptions error:&error];
     
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+    [request setURL:url];
+    [request setHTTPMethod:@"POST"];
+    [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    [request setValue:@"application/json" forHTTPHeaderField:@"Accept"];
+    [request setHTTPBody:jsonData];
+    
+    // print json:
+    NSLog(@"JSON summary: %@", [[NSString alloc] initWithData:jsonData
+                                                     encoding:NSUTF8StringEncoding]);
+    NSURLConnection *connection = [[NSURLConnection alloc] initWithRequest:request delegate:self];
+    [connection start];
+    /*
     //정보를 보낸다
     NSURL *url = [NSURL URLWithString:@"http://bowling.pineoc.cloulu.com/user/sign"];
     NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
     [request setHTTPMethod:@"POST"];
+    [request setValue:@"application/json" forKey:@"Content-Type"];
     
-    
-    NSDictionary *sendDic = @{@"email": @"i_co1022@naver.com",
-                              @"name":@"Joung Daun",
-                              @"pwd":@"merong",
-                              @"proPhoto":@"",
-                              @"ballPhoto":@""};
+    NSDictionary *sendDic = @{@"email": @"i_co1022@naver.com",@"name":@"Joung Daun",@"pwd":@"merong",@"proPhoto":@"",@"ballPhoto":@""};
     
     __autoreleasing NSError *error;
     NSData *postData = [NSJSONSerialization dataWithJSONObject:sendDic options:NSJSONWritingPrettyPrinted error:&error];
@@ -55,7 +70,7 @@
     NSURLResponse *resp = nil;
     [NSURLConnection sendSynchronousRequest:request returningResponse:&resp error:nil];
 
-    
+    */
     
  //   [self.navigationController popViewControllerAnimated:YES];
 }
